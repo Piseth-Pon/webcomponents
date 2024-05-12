31d242cb8e10a6a8746100b30215811b5670ce4b
lhuathea <hem.sokunthea2016@gmail.com>
2024-05-12 15:08:04 +0700

    proceeding the code

--- src/accordion.js
+++ src/accordion.js
@@ -1,5 +1,14 @@
 function accordion() {
-    console.log('working on accordion')
+    // Accordion 
+
+    const headers = document.querySelectorAll(".accordion-header")
+
+    headers.forEach((header) => {
+        header.addEventListener("click", (e) => {
+            const body = header.nextElementSibling
+            body.classList.toggle('show')
+        })
+    })
 }
 
 export default accordion
